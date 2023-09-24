0df79cc529133a2cf72c990773add2352acb27d2
Dani Wilson <danirae.powell@gmail.com>
2023-09-24 16:34:59 -0600

    finish testing for weather service spec

--- spec/requests/api/v0/weather_service_request_spec.rb
+++ spec/requests/api/v0/weather_service_request_spec.rb
@@ -10,8 +10,17 @@ RSpec.describe "Weather Service API", :vcr do
         coordinates = geo[:results][0][:locations][0][:latLng]
 
         weather = WeatherService.forecast_by_coordinates(coordinates)
-        x = WeatherFacade.current_weather(coordinates)
-        # require 'pry'; binding.pry
+
+        expect(weather[:location][:name]).to eq("Newport")
+        expect(weather[:location][:name]).to be_a(String)
+        expect(weather[:location][:region]).to eq("Kentucky")
+        expect(weather[:location][:region]).to be_a(String)
+        expect(weather[:location][:country]).to eq("United States of America")
+        expect(weather[:location][:region]).to be_a(String)
+        expect(weather[:location][:lat]).to eq(39.11)
+        expect(weather[:location][:lon]).to eq(-84.5)
+        expect(weather[:location][:lat]).to be_a(Float)
+        expect(weather[:location][:lon]).to be_a(Float)
       end
     end
   end
