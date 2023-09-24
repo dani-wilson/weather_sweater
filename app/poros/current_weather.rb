2821c6041770d4e2cf7c8b8cdf01dbfe6f8591c2
Dani Wilson <danirae.powell@gmail.com>
2023-09-24 16:34:29 -0600

    add poro for current weather

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -10,11 +10,11 @@ class CurrentWeather
 
   def initialize(current_weather_data)
     @last_updated = current_weather_data[:last_updated]
-    @current_temperature = current_weather_data[:current_temperature]
-    @feels_like = current_weather_data[:feels_like]
-    @humidity = current_weather_data[:humidity]
-    @uvi = current_weather_data[:uvi]
-    @visibility = current_weather_data[:visibility]
+    @current_temperature = current_weather_data[:current_temperature].to_f
+    @feels_like = current_weather_data[:feels_like].to_f
+    @humidity = current_weather_data[:humidity].to_f
+    @uvi = current_weather_data[:uvi].to_f
+    @visibility = current_weather_data[:visibility].to_f
     @current_condition = current_weather_data[:current_condition]
     @current_icon = current_weather_data[:current_icon]
   end
