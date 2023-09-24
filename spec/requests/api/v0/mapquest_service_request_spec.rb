2448ab5a3e6e0ad4db41edc597fb79bb36881ec7
Dani Wilson <danirae.powell@gmail.com>
2023-09-24 10:45:41 -0600

    amend tests

--- spec/requests/api/v0/mapquest_service_request_spec.rb
+++ spec/requests/api/v0/mapquest_service_request_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe "Mapquest Service API", :vcr do
   describe "Mapquest Service" do
     describe "happy path" do
       it "can return location data" do
-        get "/api/v0/forecast", params: { location: "cincinatti,oh" }
-        require 'pry'; binding.pry
-        expect(response.status).to eq(200)
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response["response"][0]["text"]).to be_a(String)
+        response = MapquestService.search("cincinatti, oh")
+        expect(response[:results][0][:providedLocation][:location]).to eq("cincinatti, oh")
+        expect(response[:results][0][:providedLocation][:location]).to be_a(String)
+        expect(response[:results][0][:locations][0][:latLng][:lat]).to eq(39.10713)
+        expect(response[:results][0][:locations][0][:latLng][:lng]).to eq(-84.50413)
+        expect(response[:results][0][:locations][0][:latLng][:lat]).to be_a(Float)
+        expect(response[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
       end
     end
   end
