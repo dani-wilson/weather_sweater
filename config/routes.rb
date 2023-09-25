fb29c8520d6ab22c32d0f8a98727dbeadfb90375
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 12:00:33 -0600

    redo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/api/v0/forecast', to: 'api/v0/forecast#index'
-  # get '/api/v1/book_search', to: '/api/v1/book_search#index'
+  namespace :api do
+    namespace :v1 do
+      resources :book_search, only: :index
+    end
+  end
 end
