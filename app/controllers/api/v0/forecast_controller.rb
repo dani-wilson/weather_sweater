29644c92870190b4b59efd0431b564ce710bc5f4
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 17:40:29 -0600

    edit index action

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V0::ForecastController < ApplicationController
   def index
-    
+    if params[:location].blank?
+      render json: { 'errors': 'No location specified' }, status: :bad_request
+    else
+      forecasts = WeatherFacade.weather(params[:location])
+      render json: WeatherSerializer.new(forecasts)
+    end
   end
 end
