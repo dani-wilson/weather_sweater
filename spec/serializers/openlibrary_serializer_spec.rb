9fdf9d16df8175fc411b9b75d6bb54d918107ae3
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 11:17:29 -0600

    begin testing for serializer

---
+++ spec/serializers/openlibrary_serializer_spec.rb
@@ -0,0 +1,10 @@
+# require "rails_helper"
+
+# RSpec.describe OpenlibrarySerializer do
+#   describe '#as_json' do
+#     it "returns the correct JSON structure", :vcr do
+#       books = OpenlibraryFacade.book_information("Denver, CO", 5)
+#       result = OpenlibrarySerializer.new(books).as_json
+#     end
+#   end
+# end
