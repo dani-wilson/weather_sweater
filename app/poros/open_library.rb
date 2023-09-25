8dfab1601ae6eff3a37ca6798786d7384261ded6
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 11:16:01 -0600

    edit data return

--- app/poros/open_library.rb
+++ app/poros/open_library.rb
@@ -2,12 +2,16 @@ class OpenLibrary
   attr_reader :total_books_found,
               :isbn,
               :title,
-              :destination
+              :destination,
+              :summary,
+              :temperature
   
   def initialize(book_data)
     @total_books_found = book_data[:total_books_found].to_i
     @isbn = book_data[:isbn]
     @title = book_data[:title]
     @destination = book_data[:destination]
+    @summary = book_data[:summary]
+    @temperature = book_data[:temperature].to_f
   end
 end
