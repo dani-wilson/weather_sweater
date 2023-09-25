ba4f6bf88a9e27a7ff42a66e21769eb6d135a99d
Dani Wilson <danirae.powell@gmail.com>
2023-09-24 18:31:39 -0600

    add daily weather poro

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -8,6 +8,12 @@ class DailyWeather
                 :daily_icon
 
   def initialize(daily_weather_data)
-    # @date
+    @date = daily_weather_data[:date]
+    @sunrise = daily_weather_data[:sunrise]
+    @sunset = daily_weather_data[:sunset]
+    @max_temp = daily_weather_data[:max_temp].to_f
+    @min_temp = daily_weather_data[:min_temp].to_f
+    @daily_conditions = daily_weather_data[:daily_conditions]
+    @daily_icon = daily_weather_data[:daily_icon]
   end
 end
