cda97eff95d6f2a4f3d50b29d5b336480f198f82
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 17:41:09 -0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/api/v0/forecast', to: 'api/v0/forecast#index'
+  namespace :api do
+    namespace :v0 do
+      resources :forecast, only: :index
+    end
+  end
 end
