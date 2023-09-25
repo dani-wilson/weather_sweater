3ebc1fee0e6e5a4cea1f54eab04281be04c94a28
Dani Wilson <danirae.powell@gmail.com>
2023-09-25 11:15:31 -0600

    edit get forecast method for weather call

--- app/facades/openlibrary_facade.rb
+++ app/facades/openlibrary_facade.rb
@@ -3,14 +3,21 @@ class OpenlibraryFacade
 
     def book_information(location, quantity)
       book_data = book_search(location, quantity)
+      weather = get_forecast(location)
       iterations = book_search(location, quantity)[:docs].map do |iteration|
       data = {
         total_books_found: book_data[:numFound],
         isbn: iteration[:isbn],
-        title: iteration[:title]
-        destination: iteration[:title_sort]
+        title: iteration[:title],
+        destination: iteration[:title_sort],
+        summary: weather[:current][:condition][:text],
+        temperature: weather[:current][:temp_f]
       }
       OpenLibrary.new(data)
+    end
+
+    def get_forecast(location)
+      WeatherService.forecast_by_location(location)
     end
   end
 end
