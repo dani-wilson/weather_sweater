1616ef2c90f998295ccbd725a426d692e5bd78d7
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 12:14:09 -0600

    add route for new session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :forecast, only: :index
-      resources :users, only: [:new, :create]
+      resources :users, only: :create
+      resources :sessions, only: :create
     end
   end
 end
