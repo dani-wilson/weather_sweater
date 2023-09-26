f7806c268d6d59550eea671cd0bbe90801d996fd
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 10:45:39 -0600

    finish sad path testing for user registration

--- spec/requests/api/v0/registration_request_spec.rb
+++ spec/requests/api/v0/registration_request_spec.rb
@@ -31,6 +31,72 @@ RSpec.describe "Registration Requests", :vcr do
       expect(parsed[:data][:attributes][:email]).to be_a(String)
       expect(parsed[:data][:attributes]).to have_key(:api_key)
       expect(parsed[:data][:attributes][:api_key]).to be_a(String)
+      expect(parsed[:data][:attributes]).to_not have_key(:password)
+      expect(parsed[:data][:attributes]).to_not have_key(:password_confirmation)
+    end
+
+    it "sad path - user already exists" do
+      User.create!(email: "sadpath@gmail.com", password: 'password1', password_confirmation: 'password1')
+
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+      params = {
+        "email": "sadpath@gmail.com",
+        "password": "password1",
+        "password_confirmation": "password1"
+      }
+
+      body = JSON.generate(params)
+  
+      post '/api/v0/users', headers: headers, params: body
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed[:error]).to eq('An account with this email already exists')
+    end
+
+    it "sad path - passwords don't match" do
+      params = {
+        "email": "sadpath2@gmail.com",
+        "password": "password1",
+        "password_confirmation": "password12"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+      body = JSON.generate(params)
+  
+      post '/api/v0/users', headers: headers, params: body
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed[:error]).to eq('Passwords do not match')
+    end
+
+    it "sad path - fields cannot be blank" do
+      params = {
+        "email": "sadpath3@gmail.com",
+        "password": "password1"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+      body = JSON.generate(params)
+  
+      post '/api/v0/users', headers: headers, params: body
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed[:error]).to eq('Missing required information: Fields cannot be blank')
     end
   end
 end
