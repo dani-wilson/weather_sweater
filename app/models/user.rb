335437b1f56fa8d5f958b6ff257787f135a8dbac
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 12:14:02 -0600

    add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: { require: true }
   validates :password_confirmation, presence: { require: true }
+
+  has_secure_password
 end
