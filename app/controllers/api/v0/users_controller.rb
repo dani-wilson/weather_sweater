cc1ad2ed865760c1e2c149132cb60ecc90fffdb8
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 10:45:24 -0600

    modify create action to remote .empty? method

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    if params[:email].empty? || params[:password].empty? || params[:password_confirmation].empty?
+    if !params[:email] || !params[:password] || !params[:password_confirmation]
       render json: { 'error': 'Missing required information: Fields cannot be blank' }, status: :bad_request
     elsif User.exists?(email: params[:email])
       render json: { 'error': 'An account with this email already exists' }, status: :bad_request
