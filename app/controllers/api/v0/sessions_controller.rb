8e2cde46f095672850968dca84434a6a6be0d03a
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 12:14:57 -0600

    add create action for sessions controller

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V0::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user.nil?
+      render json: { 'error': 'No such user exists' }, status: 400
+    elsif !user.nil? && user.authenticate(params[:password])
+      render json: UserSerializer.new(user)
+    else
+      render json: { 'error': 'Something went wrong' }
+    end
+  end
+end
