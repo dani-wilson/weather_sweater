26b9502741a556fab5d1437466ec89d269d9ecff
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 19:05:48 -0600

    add road trip controller and create action

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,5 +1,15 @@
 class Api::V0::RoadTripController < ApplicationController
   def create
-    
+    if params[:origin].blank? || params[:destination].blank? || params[:api_key].blank?
+      render json: { 'error': 'Fields cannot be blank' }
+    else
+      render json: RoadTripSerializer.new(roadtrip), status: 201
+    end
+  end
+
+  private
+
+  def roadtrip
+    RoadTripFacade.road_trip(params[:origin], params[:destination])
   end
 end
