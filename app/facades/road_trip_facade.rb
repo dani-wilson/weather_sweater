51579fc7f3faebf964434d46e3842f35177f09d7
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 22:26:23 -0600

    complete testing for sad path

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -12,19 +12,23 @@ class RoadTripFacade
     end
 
     def future_weather(start_city, end_city)
-      hours = ((travel_time(start_city, end_city).to_i.round) / 60) / 60
-      seconds = hours * 3600
-      date = DateTime.now + Rational(seconds, 86400) + 14
-      eta = date.strftime('%Y-%m-%d')
-      response = WeatherService.weather_by_eta(end_city, eta)
-      parsed = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
-      data = {
-        # datetime: parsed[:location][:localtime],
-        # temperature: parsed[:forecast][:forecastday][0][:day][:maxtemp_f],
-        # conditions: parsed[:forecast][:forecastday][0][:day][:condition][:text]
-      }
-      RoadTrip.new(data)
+      t_time = travel_time(start_city, end_city)
+      if t_time != "impossible"
+        hours = ((travel_time(start_city, end_city).to_i.round) / 60) / 60
+        seconds = hours * 3600
+        date = DateTime.now + Rational(seconds, 86400) + 14
+        eta = date.strftime('%Y-%m-%d')
+        response = WeatherService.weather_by_eta(end_city, eta)
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        data = {
+          datetime: parsed[:location][:localtime],
+          temperature: parsed[:forecast][:forecastday][0][:day][:maxtemp_f],
+          conditions: parsed[:forecast][:forecastday][0][:day][:condition][:text]
+        }
+        RoadTrip.new(data)
+      else 
+        return 'impossible'
+      end
     end
 
     def future_date(start_city, end_city)
@@ -36,7 +40,7 @@ class RoadTripFacade
     def travel_time(start_city, end_city)
       time = MapquestService.estimated_time(start_city, end_city)
       if time[:info][:messages] == [ "No routes found." ]
-        'impossible'
+        return 'impossible'
       else
         total_hours = time[:time][1].round
       end
