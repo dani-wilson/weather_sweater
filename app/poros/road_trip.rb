88b2d4ac49bf038f522d72178133af404df274c8
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 21:31:49 -0600

    add everything from road trip forecast poro into this one

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -7,43 +7,25 @@ class RoadTrip
   def initialize(roadtrip_data)
     @start_city = roadtrip_data[:start_city]
     @end_city = roadtrip_data[:end_city]
-    @travel_time = roadtrip_data[:travel_time]
-    @weather_at_eta = roadtrip_data[:weather_at_eta]
+    @travel_time = format_travel_time(roadtrip_data[:travel_time])
+    @weather_at_eta = format_weather(roadtrip_data)
   end
 
+  def format_travel_time(roadtrip_data)
+    if roadtrip_data == "impossible"
+      nil 
+    else
+      hours = (roadtrip_data.to_i / 3600).to_i
+      minutes = ((roadtrip_data.to_i % 3600) / 60).to_i
+      "#{hours} H #{minutes} m"
+    end
+  end
 
-  # def format_travel_time(roadtrip_data)
-  #   if roadtrip_data[:travel_time] == 'impossible'
-  #     nil
-  #   else
-  #     roadtrip_data[:travel_time]
-  #   end
-  # end
-
-  # def formatted_travel_time(roadtrip_data)
-  #   if roadtrip_data == 'impossible'
-  #     'impossible'
-  #   else
-  #     minutes = roadtrip_data.to_f / 60
-  #     hours = (minutes / 60).to_i 
-  #     fractal_hours = (minutes / 60).round(2).modulo(1)
-  #     fractal_minutes = (fractal_hours * 60).round
-
-  #     pleuralize_hours = 
-  #     if hours == 1
-  #       'hour'
-  #     else
-  #       'hours'
-  #     end
-
-  #     pleuralize_minutes = 
-  #     if fractal_minutes == 1
-  #       'minute'
-  #     else
-  #       'minutes'
-  #     end
-
-  #     "#{hours} #{pleuralize_hours}, #{fractal_minutes} #{pleuralize_minutes}"
-  #   end
-  # end
+  def format_weather(roadtrip_data)
+    if roadtrip_data[:travel_time] == "impossible"
+      'impossible'
+    else
+      roadtrip_data[:weather_at_eta]
+    end
+  end
 end
