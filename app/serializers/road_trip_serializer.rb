bdfba2c638aaf12c7a9ee056cecbe045d9b04b08
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 19:06:27 -0600

    add road trip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,5 +1,6 @@
 class RoadTripSerializer
 include JSONAPI::Serializer
-set_type: "road_trip"
-attributes: :start_city, :end_city, :travel_time, :weather_at_eta
+set_type :road_trip
+set_id 'nil?'
+attributes :start_city, :end_city, :travel_time, :weather_at_eta
 end
