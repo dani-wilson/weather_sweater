dcc53f961a041d57d746acb14a9e8d8c000da981
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 21:32:18 -0600

    add tests that were in the road trip forecast spec

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe RoadTrip do
     data = {
       "start_city": "Vernal,UT",
       "end_city": "Astoria,OR",
-      "travel_time": "14 hours 0 minutes",
+      "travel_time": "53708",
       "weather_at_eta": {
+        "datetime": "2023-09-26 20:07",
         "conditions": "light rain",
         "temperature": "42.1"
       }
@@ -17,7 +18,8 @@ RSpec.describe RoadTrip do
     expect(road_trip).to be_a RoadTrip
     expect(road_trip.start_city).to eq("Vernal,UT")
     expect(road_trip.end_city).to eq("Astoria,OR")
-    expect(road_trip.travel_time).to eq("14 hours 0 minutes")
+    expect(road_trip.travel_time).to eq("14 H 55 m")
+    expect(road_trip.weather_at_eta[:datetime]).to eq("2023-09-26 20:07")
     expect(road_trip.weather_at_eta[:conditions]).to eq("light rain")
     expect(road_trip.weather_at_eta[:temperature]).to eq("42.1")
   end
