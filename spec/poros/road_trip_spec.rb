527a2f0a644b4e8c94b2c1c0b862840d1503c60e
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 19:07:58 -0600

    add road trip poro spec

---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe RoadTrip do
+  it "exists and has attributes" do
+    data = {
+      "start_city": "Vernal,UT",
+      "end_city": "Astoria,OR",
+      "travel_time": "14 hours 0 minutes",
+      "weather_at_eta": {
+        "conditions": "light rain",
+        "temperature": "42.1"
+      }
+    }
+
+    road_trip = RoadTrip.new(data)
+
+    expect(road_trip).to be_a RoadTrip
+    expect(road_trip.start_city).to eq("Vernal,UT")
+    expect(road_trip.end_city).to eq("Astoria,OR")
+    expect(road_trip.travel_time).to eq("14 hours 0 minutes")
+    require 'pry'; binding.pry
+    expect(road_trip.weather_at_eta[:conditions]).to eq("light rain")
+    expect(road_trip.weather_at_eta[:temperature]).to eq(42.1)
+  end
+end
