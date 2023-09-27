82d8a8fad09c16c58be469d12a7e14a68fef8a3d
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 21:31:57 -0600

    remove file

--- app/poros/road_trip_forecast.rb
+++
@@ -1,9 +0,0 @@
-class RoadTripForecast
-  attr_reader :temperature,
-              :conditions
-
-  def initialize(params)
-    @temperature = params[:temperature].to_f
-    @conditions = params[:conditions]
-  end
-end
