c17bad76788bc0eb0e5599adf0dbbdd5fe4af60a
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 19:37:45 -0600

    edits

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -8,7 +8,7 @@ class RoadTrip
     @start_city = roadtrip_data[:start_city]
     @end_city = roadtrip_data[:end_city]
     @travel_time = roadtrip_data[:travel_time]
-    @weather_at_eta = roadtrip_data[:future_weather]
+    @weather_at_eta = roadtrip_data[:weather_at_eta]
   end
 
 
