dba0e5308b33f92029a9493962082bae6a3fa6ea
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 21:30:52 -0600

    modify road trip controller

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::RoadTripController < ApplicationController
   def create
     if params[:origin].blank? || params[:destination].blank? || params[:api_key].blank?
-      render json: { 'error': 'Fields cannot be blank' }
+      render json: { 'error': 'Fields cannot be blank' }, status: 401
     else
       render json: RoadTripSerializer.new(roadtrip), status: 201
     end
