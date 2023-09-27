01e48f4ede9af72b39912e1083296e65d0c4e886
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 21:31:08 -0600

    add more to future weather

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -12,19 +12,33 @@ class RoadTripFacade
     end
 
     def future_weather(start_city, end_city)
-      time = travel_time(start_city, end_city).round
-      unformatted_date = DateTime.now + time 
-      eta = unformatted_date.strftime('%m/%d/%Y')
-      weather = WeatherService.weather_by_eta(end_city, eta)
-      RoadTripForecast.new(weather)
+      hours = ((travel_time(start_city, end_city).to_i.round) / 60) / 60
+      seconds = hours * 3600
+      date = DateTime.now + Rational(seconds, 86400) + 14
+      eta = date.strftime('%Y-%m-%d')
+      response = WeatherService.weather_by_eta(end_city, eta)
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      require 'pry'; binding.pry
+      data = {
+        # datetime: parsed[:location][:localtime],
+        # temperature: parsed[:forecast][:forecastday][0][:day][:maxtemp_f],
+        # conditions: parsed[:forecast][:forecastday][0][:day][:condition][:text]
+      }
+      RoadTrip.new(data)
+    end
+
+    def future_date(start_city, end_city)
+      time = travel_time(start_city, end_city)
+      unformatted_date = DateTime.now + time
+      eta = unformatted_date.strftime('%m/%d/%Y %H%M')
     end
 
     def travel_time(start_city, end_city)
       time = MapquestService.estimated_time(start_city, end_city)
-      if time[:info][:messages] == [ "We are unable to route with the given locations" ]
+      if time[:info][:messages] == [ "No routes found." ]
         'impossible'
       else
-        total_hours = ((time[:time][1]).round / 60) / 60
+        total_hours = time[:time][1].round
       end
     end
   end
