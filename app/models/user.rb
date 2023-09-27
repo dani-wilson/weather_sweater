02a20c4d72b85ec06744a9901795e02e65996f3e
Dani Wilson <danirae.powell@gmail.com>
2023-09-26 19:06:17 -0600

    remove password digest from table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: { require: true }
   validates :password_confirmation, presence: { require: true }
-
-  has_secure_password
 end
